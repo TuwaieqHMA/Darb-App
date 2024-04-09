0b8cfdb893d3863c318ff0396159152fb4376fcd
hmody360 <s441010207@st.uqu.edu.sa>
2024-04-09 14:04:38 +0300

    Fix: Fixed Signup pages UI

--- lib/pages/create_account_supervisor_page.dart
+++ lib/pages/create_account_supervisor_page.dart
@@ -22,54 +22,15 @@ class CreateAccountSupervisorPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: SingleChildScrollView(
-          child: SizedBox(
-            width: context.getWidth(),
-            height: context.getHeight(),
-            child: Stack(
+        child: ListView(
+          children: [
+            Stack(
               children: [
-                Align(
-                  alignment: Alignment.bottomCenter,
-                  child: Container(
-                    width: context.getWidth(),
-                    height: context.getHeight() * .63,
-                    padding: const EdgeInsets.only(
-                        left: 16, right: 16, top: 16, bottom: 16),
-                    decoration: const BoxDecoration(
-                        color: signatureBlueColor,
-                        borderRadius: BorderRadius.only(
-                            topLeft: Radius.circular(20),
-                            topRight: Radius.circular(20))),
-                    child: ListView(
-                      shrinkWrap: true,
-                      children: [
-                        HeaderTextField(controller: nameController, headerText: "الاسم",),
-                        height8,
-                        HeaderTextField(controller: phoneController, headerText: "رقم الجوال", keyboardType: TextInputType.number,),
-                        height8,
-                        HeaderTextField(controller: emailController, headerText: "البريد الالكتروني",),
-                        height8,
-                        HeaderTextField(controller: passwordController, headerText: "كلمة المرور", isObscured: true),
-                        height8,
-                        HeaderTextField(controller: rePasswordController, headerText: "تأكيد كلمة المرور", isObscured: true),
-                        height32,
-                        BottomButton(text: "إنشاء الحساب", onPressed: () {
-                          
-                        },),
-                        height16,
-                        TwoTextSpan(normalText: "لديك حساب؟      ", underlinedText: "تسجيل الدخول", onTap: () {
-                          context.push(LoginPage(), false);
-                        },),
-                        height16,
-                      ],
-                    ),
-                  ),
-                ),
                 Align(
                   alignment: Alignment.topCenter,
                   child: Image.asset(
                     "assets/images/supervisor_vector.png",
-                    scale: 5.8,
+                    width: (context.getWidth() < 786) ? context.getWidth() : 786,
                   ),
                 ),
                 const Align(
@@ -85,7 +46,37 @@ class CreateAccountSupervisorPage extends StatelessWidget {
                     ))
               ],
             ),
-          ),
+            Container(
+              width: context.getWidth(),
+              padding: const EdgeInsets.only(
+                  left: 16, right: 16, top: 16, bottom: 16),
+              decoration: const BoxDecoration(
+                  color: signatureBlueColor,
+                  borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(20),
+                      topRight: Radius.circular(20))),
+              child: Column(
+                children: [
+                  HeaderTextField(controller: nameController, headerText: "الاسم",),
+                  height8,
+                  HeaderTextField(controller: phoneController, headerText: "رقم الجوال", keyboardType: TextInputType.number,),
+                  height8,
+                  HeaderTextField(controller: emailController, headerText: "البريد الالكتروني",),
+                  height8,
+                  HeaderTextField(controller: passwordController, headerText: "كلمة المرور", isObscured: true),
+                  height8,
+                  HeaderTextField(controller: rePasswordController, headerText: "تأكيد كلمة المرور", isObscured: true),
+                  height32,
+                  BottomButton(text: "إنشاء الحساب", onPressed: () {
+                  },),
+                  height16,
+                  TwoTextSpan(normalText: "لديك حساب؟      ", underlinedText: "تسجيل الدخول", onTap: () {
+                    context.push(LoginPage(), false);
+                  },),
+                ],
+              ),
+            ),
+          ]
         ),
       ),
     );
